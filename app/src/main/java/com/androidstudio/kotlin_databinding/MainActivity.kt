51e8f71a941b1621f667f8d30e6366bfa3667abf
Guilherme Souza <73115455+gsouzahh@users.noreply.github.com>
2021-07-20 09:57:29 -0300

    sl

--- app/src/main/java/com/androidstudio/kotlin_databinding/MainActivity.kt
+++ app/src/main/java/com/androidstudio/kotlin_databinding/MainActivity.kt
@@ -2,10 +2,20 @@ package com.androidstudio.kotlin_databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.androidstudio.kotlin_databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.viewmodel = viewModel
+
+        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
     }
 }
--- app/src/main/java/com/androidstudio/kotlin_databinding/MainViewModel.kt
+++ app/src/main/java/com/androidstudio/kotlin_databinding/MainViewModel.kt
@@ -6,7 +6,14 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
 
+    private var number = 1
     private val m_contador = MutableLiveData<Int>()
     val contador: LiveData<Int> get() = m_contador
 
+    init {
+        m_contador.value = number
+    }
+    fun clickMe(){
+        m_contador.value = ++number
+    }
 }
